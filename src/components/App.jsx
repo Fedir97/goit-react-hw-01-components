d9aa0077fe404e2d700c68ff5d5ad1585d129bb3
Fedir97 <fedorcuprov97@gmail.com>
2023-01-17 22:18:19 +0300

    solo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Profile from "./WebProfile/Profile";
+import user from "../assets/user.json"
+import Statistics from "./Statistics/Statistics"
+import data from "../assets/data.json"
+import FriendsList from "./FriendsList/Friends"
+import friends from "../assets/friends.json"
+import TransactionHistory from "./TransactionHistory/TransactionHistory"
+import transactions from "../assets/transactions.json"
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+        <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+<Statistics title="Upload stats" stats={data} />
+<FriendsList friends={friends} />;
+<TransactionHistory items={transactions} />;
     </div>
   );
 };
+
+
+
